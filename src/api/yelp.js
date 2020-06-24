679e859db2949ff59adcb1b99eb932fefb44f36e
taeism <taeism@gmail.com>
2020-06-24 16:01:45 -0400

    Update yelp.js

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses/',
-    headers: {
-        Authorization: 'Bearer 8v2hXEbPsWfc9YTqU7OS2lEspgQeelZoy9OwMX1LGZZAO5nL5jwimPQpuRyttgQXFuyDU5KB9A5mPSv6f-mBB-Iup_PChPo5BICHBgS-HQ4QpIgOeXG-XIn_x2byXnYx'
+    headers: 
+        Authorization: 'Bearer '
     }
 })
